0aaa5630fe1779f9aaa80ca63ae8f78798fb1a06
jobsons10 <jobsonsobrinho@grupootg.com>
2023-01-15 13:21:39 -0400

    div why use

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-const scrollSectionRight = function(){
-    const usersWrapper = document.querySelector('.users-wrapper');
-    usersWrapper.scroll(100, 0);
+const column1 = document.getElementById("column-1");
+const column2 = document.getElementById("column-2");
+const column3 = document.getElementById("column-3");
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+let currentColumn = 1;
+
+leftArrow.addEventListener("click", previousColumn);
+rightArrow.addEventListener("click", nextColumn);
+
+function previousColumn() {
+  if (currentColumn === 1) {
+    currentColumn = 3;
+  } else {
+    currentColumn--;
+  }
+
+  updateColumn();
 }
 
-const scrollSectionLeft = function(){
-    const usersWrapper = document.querySelector('.users-wrapper');
-    usersWrapper.scroll({
-        top: 0,
-        left: -100,
-    });
+function nextColumn() {
+  if (currentColumn === 3) {
+    currentColumn = 1;
+  } else {
+    currentColumn++;
+  }
+
+  updateColumn();
 }
+
+function updateColumn() {
+    column1.classList.remove("active");
+    column2.classList.remove("active");
+    column3.classList.remove("active");
+    if (currentColumn === 1) {
+      column1.classList.add("active");
+    } else if (currentColumn === 2) {
+      column2.classList.add("active");
+    } else {
+      column3.classList.add("active");
+    }
+  }
